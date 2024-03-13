58b73cb9ce82307461d6e12ba708354abdac02d7
ShySpaceSheep <theminecraftf303@gmail.com>
2024-03-13 23:56:08 +0800

    Changed implementation of language selector

--- src/utils/LangSelector.ts
+++ src/utils/LangSelector.ts
@@ -2,18 +2,21 @@ import 'dotenv/config';
 import commons = require('../../lang/lang-common.json');
 
 const SUPPORTED_LANGUAGES: string[] = ["EN", "JP"];
-let configLanguage: string = process.env.LANGUAGE;
 
-if (configLanguage !== "EN") { console.log(commons.log.task.verifyingLang.replace('{LANGUAGE}', configLanguage)); }
-if (SUPPORTED_LANGUAGES.includes(configLanguage)) {
-    console.log(commons.log.success.onValidLanguage.replace('{LANGUAGE}', configLanguage));
-} else {
-    console.warn(`\u001b[31m` + commons.log.warning.invalidLanguage.replace('{LANGUAGE}', configLanguage) + `\u001b[0m`);
-    configLanguage = "EN";
+export function setGlobalLanguage(): string {
+    let configLanguage: string = process.env.LANGUAGE;
+
+    if (configLanguage !== "EN") { console.log(commons.log.task.verifyingLang.replace('{LANGUAGE}', configLanguage)); }
+    if (SUPPORTED_LANGUAGES.includes(configLanguage)) {
+        console.log(commons.log.success.onValidLanguage.replace('{LANGUAGE}', configLanguage));
+        return configLanguage;
+    } else {
+        console.warn(`\u001b[31m` + commons.log.warning.invalidLanguage.replace('{LANGUAGE}', configLanguage) + `\u001b[0m`);
+        return "EN";
+    }
 }
 
-const BOT_LANGUAGE: string = configLanguage;
-export function getLangJSONText(keyword: string) : string {
-    const languageFile = require(`./../../lang/lang-${BOT_LANGUAGE}.json`);
+export function getLangJSONText(keyword: string, language: string) : string {
+    const languageFile = require(`./../../lang/lang-${language}.json`);
     return languageFile[keyword];
 }
